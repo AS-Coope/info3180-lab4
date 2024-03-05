ac8cbb90fe26d3bbc1372eb42632b6010a9d45ff
AS-Coope <deltazirconium@gmail.com>
2024-03-05 09:26:20 -0500

    create variables to capture the values of username and password

--- app/views.py
+++ app/views.py
@@ -45,7 +45,8 @@ def login():
     # and not just one field
     if form.username.data:
         # Get the username and password values from the form.
-
+        username = form.username.data
+        password = form.password.data
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
